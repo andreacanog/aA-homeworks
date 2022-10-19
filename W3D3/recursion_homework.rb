d67c2fc388d7ebf2a6f4abb0b0f5151bc9e37c7a
Andrea Cano <andreacanogisbert@gmail.com>
2022-10-19 06:13:17 -0700

    finish hw

--- W3D3/recursion_homework.rb
+++ W3D3/recursion_homework.rb
@@ -1,43 +1,58 @@
 # Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n)
 def sum_to(n)
+    return 1 if n == 1
+    return nil if n < 0
 
+    n + sum_to(n - 1)
 end
 
-sum_to(5)  # => returns 15
-sum_to(1)  # => returns 1
-sum_to(9)  # => returns 45
-sum_to(-8)  # => returns nil
+p sum_to(5)  # => returns 15
+p sum_to(1)  # => returns 1
+p sum_to(9)  # => returns 45
+p sum_to(-8)  # => returns nil
 
+p "-------------------------------------------------"
 
 #Write a function add_numbers(nums_array) that takes in an array of Integers and returns 
 #the sum of those numbers. Write this method recursively.
 
 def add_numbers(nums_array)
+    return nums_array.last if nums_array.length <= 1 
+
+    first = nums_array.first
+    sum = first + add_numbers(nums_array[1..-1])
+    sum 
 end
 
 
 
-add_numbers([1,2,3,4]) # => returns 10
-add_numbers([3]) # => returns 3
-add_numbers([-80,34,7]) # => returns -39
-add_numbers([]) # => returns nil
+p add_numbers([1,2,3,4]) # => returns 10
+p add_numbers([3]) # => returns 3
+p add_numbers([-80,34,7]) # => returns -39
+p add_numbers([]) # => returns nil
 
-
+p "-------------------------------------------------"
 
 #Let's write a method that will solve Gamma Function recursively. The Gamma Function is defined Γ(n) = (n-1)!.
 
 def gamma_fnc(n)
+    return 1 if n == 1
+    return nil if n <= 0
 
+    (n - 1) * gamma_fnc(n - 1)
 end
 
 
-gamma_fnc(0)  # => returns nil
-gamma_fnc(1)  # => returns 1
-gamma_fnc(4)  # => returns 6
-gamma_fnc(8)  # => returns 5040
 
 
 
+p gamma_fnc(0)  # => returns nil
+p gamma_fnc(1)  # => returns 1
+p gamma_fnc(4)  # => returns 6
+p gamma_fnc(8)  # => returns 5040
+
+
+p "-------------------------------------------------"
 
 #Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available 
 #at the ice cream shop, as well as the user's favorite ice cream flavor. Recursively find out whether or not 
@@ -45,29 +60,36 @@ gamma_fnc(8)  # => returns 5040
 
 
 def ice_cream_shop(flavors, favorite)
+    return false if flavors.empty?
+    return true if flavors.last == favorite
 
-
+    ice_cream_shop(flavors[0...-1], favorite)
 end
 
 
-ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
-ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
-ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
-ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
-ice_cream_shop([], 'honey lavender')  # => returns false
+p ice_cream_shop(['vanilla', 'strawberry'], 'blue moon')  # => returns false
+p ice_cream_shop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea')  # => returns true
+p ice_cream_shop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio')  # => returns false
+p ice_cream_shop(['moose tracks'], 'moose tracks')  # => returns true
+p ice_cream_shop([], 'honey lavender')  # => returns false
 
 
+p "-------------------------------------------------"
 
 #Write a function reverse(string) that takes in a string and returns it reversed.
 
 def reverse(string)
+    return string if string.length <= 1
+    return nil if string == ""
+
+    string[-1] + reverse(string[0..-2])
 
 end
 
 
-reverse("house") # => "esuoh"
-reverse("dog") # => "god"
-reverse("atom") # => "mota"
-reverse("q") # => "q"
-reverse("id") # => "di"
-reverse("") # => ""
+p reverse("house") # => "esuoh"
+p reverse("dog") # => "god"
+p reverse("atom") # => "mota"
+p reverse("q") # => "q"
+p reverse("id") # => "di"
+p reverse("") # => ""
